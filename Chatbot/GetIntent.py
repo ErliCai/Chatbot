37dfff365b8cf0de013795a1b2d748a46bbf2c61
ErliCai <53851846+ErliCai@users.noreply.github.com>
2020-01-30 00:02:05 +0000

    Update GetIntent.py

--- Chatbot/GetIntent.py
+++ Chatbot/GetIntent.py
@@ -6,6 +6,9 @@ Created on Sun Jan 26 15:17:21 2020
 @author: cai
 """
 
+#Combine EasyIntent and ComplexIntent
+# Search for Easy Intent first because it;s quicker. If find anything we could skip uising ComplexIntent
+
 from EasyIntent import easy_intent
 from ComplexIntent import complex_intent
 
